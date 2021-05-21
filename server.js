a97dfd017401f0238b1f8cb533e23f1cabbcb1b8
TheSilkky <silkky@silkky.cloud>
2021-05-21 14:52:01 +1000

    Upgraded to Bootstrap 5

--- server.js
+++ server.js
@@ -7,16 +7,20 @@ const HOST = '0.0.0.0';
 // Node App
 var express = require('express');
 const ejs = require('ejs');
+const path = require('path')
 // Initialize Express
 var app = express();
 // Render static files
 app.use(express.static('public'));
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
-// Port website will run on
 
-app.listen(PORT, HOST);
-console.log(`Silkky.Cloud is alive at http://${HOST}:${PORT}!`);
+// Bootstrap
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/dist/cjs')))
+// Icons
+app.use('/assets/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/icons')))
 
 // GET Routes
 // Root Directory
@@ -48,3 +52,7 @@ app.get('/privacy', function (req, res) {
 app.get('/legal', function (req, res) {
   res.render('pages/legal')
 });
+
+// Start App
+app.listen(PORT, HOST);
+console.log(`Silkky.Cloud is alive at http://${HOST}:${PORT}!`);
