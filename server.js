896d9d303d6e16a4376d8af4c46e8554f3c78e16
TheSilkky <silkky@silkky.cloud>
2021-05-30 19:54:39 +1000

    Improve server.js layout

--- server.js
+++ server.js
@@ -1,73 +1,63 @@
 'use strict';
 
-// Constraints
+// Host
 const PORT = '8080';
 const HOST = '0.0.0.0';
-
-// Node App
-var express = require('express');
-var helmet = require('helmet');
+// App
+const express = require('express');
+const helmet = require('helmet');
 const pug = require('pug');
 const path = require('path')
-// Initialize Express
-var app = express();
-// Set static directory
+
+// 
+const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-// Set views directory for pug
 app.set('views', './views')
-// Set the view engine to pug
 app.set('view engine', 'pug');
 
-// Helmet
+// Static files 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/dist/esm')))
+app.use('/assets/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/icons')))
+
+// Web Server
+// This disables the `contentSecurityPolicy` middleware but keeps the rest.
 app.use(
   helmet({
     contentSecurityPolicy: false,
   })
 );
 
-// Bootstrap
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/dist/esm')))
-// Icons
-app.use('/assets/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/icons')))
-
-// GET Routes
 // Root Directory
 app.get('/', function (req, res) {
   res.render('pages/index')
 });
-
 // Donate Directory
 app.get('/contribute', function (req, res) {
   res.render('pages/contribute')
 });
-
 // Help Directory
 app.get('/help', function (req, res) {
   res.render('pages/help')
 });
-
 // About Directory
 app.get('/about', function (req, res) {
   res.render('pages/about')
 });
-
 // Terms of Service Directory
 app.get('/tos', function (req, res) {
   res.render('pages/tos')
 });
-
 // Privacy Policy Directory
 app.get('/privacy', function (req, res) {
   res.render('pages/privacy')
 });
-
 // Legal Notice Directory
 app.get('/legal', function (req, res) {
   res.render('pages/legal')
 });
-
 // Error Handling
 // 404
 app.use(function (req, res, next) {
@@ -80,6 +70,6 @@ app.use(function (err, req, res, next) {
   res.send('500 Server Error')
 })
 
-// Start App
+// Start
 app.listen(PORT, HOST);
 console.log(`Silkky.Cloud is alive at http://${HOST}:${PORT}!`);
