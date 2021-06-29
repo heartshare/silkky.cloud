b19bed856f92f13f9f00dad3cffb0fcb462bf894
Oscar <silkky@silkky.cloud>
2021-06-29 18:48:48 +1000

    Fix typos, fix local development

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 'use strict';
-
-// Host
-const PORT = '8080';
-const HOST = '0.0.0.0';
-// App constraints
 const express = require('express');
 const helmet = require('helmet');
-const path = require('path')
+const path = require('path');
 
-// App
+function hostCheck() {
+  if (process.env.NODE_ENV === 'production') {
+    return '0.0.0.0'
+  }
+  else {
+    return '127.0.0.1'
+  }
+}
+// Constraints
+const port = 8080;
+const host = hostCheck();
+
 const app = express();
-app.set('views', './views')
+app.set('views', './views');
 app.set('view engine', 'pug');
 
 // Static files 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/dist/esm')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/dist/esm')));
 
-// Web Server
 // This disables the `contentSecurityPolicy` middleware but keeps the rest.
 app.use(
   helmet({
@@ -27,46 +32,48 @@ app.use(
   })
 );
 
+// Routes
 // Root Directory
 app.get('/', function (req, res) {
-  res.render('pages/index')
+  res.render('pages/index');
 });
 // Donate Directory
 app.get('/contribute', function (req, res) {
-  res.render('pages/contribute')
+  res.render('pages/contribute');
 });
 // Help Directory
 app.get('/help', function (req, res) {
-  res.render('pages/help')
+  res.render('pages/help');
 });
 // About Directory
 app.get('/about', function (req, res) {
-  res.render('pages/about')
+  res.render('pages/about');
 });
 // Terms of Service Directory
 app.get('/tos', function (req, res) {
-  res.render('pages/tos')
+  res.render('pages/tos');
 });
 // Privacy Policy Directory
 app.get('/privacy', function (req, res) {
-  res.render('pages/privacy')
+  res.render('pages/privacy');
 });
 // Legal Notice Directory
 app.get('/legal', function (req, res) {
-  res.render('pages/legal')
+  res.render('pages/legal');
 });
 // Error Handling
 // 404
 app.use(function (req, res, next) {
-  res.status(404).render("error/404")
+  res.status(404).render("error/404");
 })
 // 500
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).render('error/500')
-  res.send('500 Server Error')
-})
+app.use(function (req, res, err, next) {
+  console.error(err.stack);
+  res.status(500).render('error/500');
+  res.send('500 Server Error');
+});
 
 // Start
-app.listen(PORT, HOST);
-console.log(`Silkky.Cloud is alive at http://${HOST}:${PORT}!`);
+app.listen(port, host, () => {
+  console.log(`Silkky.Cloud is alive at http://${host}:${port}!`);
+});
