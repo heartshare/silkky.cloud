cb641645f5cd65307d2121b53b478e9b76c33374
Oscar <silkky@silkky.cloud>
2021-07-22 00:20:40 +1000

    Add contribute page layout

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const upath = require('upath');
 
 const port = '8080';
-const host = '192.168.1.118';
 const app = express();
 
 app.use(express.static(upath.join(process.cwd(), 'dist'), {
@@ -15,5 +14,6 @@ app.use(function(req, res, next) {
     res.sendFile(upath.join(process.cwd(), 'dist/404.html'));
 });
 
-app.listen(port, host);
-console.log(`THE SUPER FUCKING BORING PROJECT is alive at http://${host}:${port}`);
+app.listen(port, () => {
+    console.log(`THE SUPER FUCKING BORING PROJECT is alive at http://localhost:${port}`);
+});
